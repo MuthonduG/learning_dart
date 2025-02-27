d7b284caee7aab48a037084b618a20aba78dcc16
MuthonduG <muthondugithinji@gmail.com>
2025-02-27 09:10:13 -0500

    updates: maps

--- maps.dart
+++ maps.dart
@@ -1,4 +1,30 @@
 void main() {
   // maps
-  
+  var toppings = {"john": "Peporoni", "Mary": "chhese"};
+  print(toppings);
+  print(toppings["john"]);
+
+  // show values
+  print(toppings.values);
+
+  // show keys
+  print(toppings.keys);
+
+  // show length
+  print(toppings.length);
+
+  // add an element
+  toppings["tim"] = "Sausage";
+
+  // add multiple items
+  toppings.addAll({"Tina": "new", "Steve": "nothing"});
+  print(toppings);
+
+  // remove single element
+  toppings.remove("john");
+  print(toppings);
+
+  // remove everythin
+  toppings.clear();
+  print(toppings);
 }
